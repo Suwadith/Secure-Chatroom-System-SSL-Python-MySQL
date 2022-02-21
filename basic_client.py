62bfc6e43e1e0f0594c8fd7e47cf193b236f9c4c
Suwadith <suwadith.srithar@gmail.com>
2022-02-21 20:41:24 +0000

    admin functionalities implemented on the basic client & server implementations TODO: Menu & Private chat

--- basic_client.py
+++ basic_client.py
@@ -11,16 +11,32 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((host_address, port_number))
 
 username = input("Choose a username: ")
+if username == 'admin':
+    password = input("Enter password for admin: ")
 
+stop_thread = False
 
 # Receive messages from the server
 def receive():
     while True:
+        global stop_thread
+        if stop_thread:
+            break
         # Check if it's a message or an initial username input
         try:
             message = client.recv(1024).decode('ascii')
             if message == 'USERNAME':
                 client.send(username.encode('ascii'))
+                next_message = client.recv(1024).decode('ascii')
+                if next_message == 'PASS':
+                    client.send(password.encode('ascii'))
+                    if client.recv(1024).decode('ascii') == 'REFUSE':
+                        print("Connection was refused! Wrong password!")
+                        stop_thread = True
+                elif next_message == 'BAN':
+                    print("Connection refused because of ban!")
+                    client.close()
+                    stop_thread = True
             else:
                 print(message)
         # Close connection if any errors occurred
@@ -33,8 +49,21 @@ def receive():
 # Accepting inputs from user constantly
 def write():
     while True:
+        if stop_thread:
+            break
         message = str(username) + ": " + str(input(""))
-        client.send(message.encode('ascii'))
+        if message[len(username)+2:].startswith('/'):
+            if username == 'admin':
+                if message[len(username)+2:].startswith('/kick'):
+                    client.send(("KICK " + message[len(username)+2+6:]).encode('ascii'))
+                    # print(("KICK " + message[len(username)+2+6:]).encode('ascii'))
+                elif message[len(username)+2:].startswith('/ban'):
+                    client.send(("BAN " + message[len(username)+2+5:]).encode('ascii'))
+                # print("in")
+            else:
+                print("Commands can only be executed by the admin")
+        else:
+            client.send(message.encode('ascii'))
 
 
 receive_thread = threading.Thread(target=receive)
--- basic_server.py
+++ basic_server.py
@@ -27,17 +27,34 @@ def handle(client):
     while True:
         # If possible announce the message to everyone on the chatroom
         try:
-            message = client.recv(1024)
-            announce(message)
+            msg = message = client.recv(1024)
+            if msg.decode('ascii').startswith('KICK'):
+                if usernames[clients.index(client)] == 'admin':
+                    name_to_kick = msg.decode('ascii')[5:]
+                    kick_user(name_to_kick)
+                    # print(name_to_kick)
+                else:
+                    client.send('Command was refused!'.encode('ascii'))
+            elif msg.decode('ascii').startswith('BAN'):
+                if usernames[clients.index(client)] == 'admin':
+                    name_to_ban = msg.decode('ascii')[4:]
+                    kick_user(name_to_ban)
+                    # ban_user(name_to_ban)
+                    # print(name_to_ban + "has been banned!")
+                else:
+                    client.send('Command was refused!'.encode('ascii'))
+            else:
+                announce(message)
         # If not possible (exception thrown) then cut the connection and broadcast that the user has left
         except:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            username = usernames[index]
-            announce((str(username) + " left the chat!").encode('ascii'))
-            usernames.remove(username)
-            break
+            if client in clients:
+                index = clients.index(client)
+                clients.remove(client)
+                client.close()
+                username = usernames[index]
+                announce((str(username) + " left the chat!").encode('ascii'))
+                usernames.remove(username)
+                break
 
 
 # Accept connections from clients to the server
@@ -49,6 +66,17 @@ def receive():
         # Get the username from the client and store it
         client.send("USERNAME".encode('ascii'))
         username = client.recv(1024).decode('ascii')
+
+        # new
+        if username == 'admin':
+            client.send('PASS'.encode('ascii'))
+            password = client.recv(1024).decode('ascii')
+
+            if password != 'adminpass':
+                client.send('REFUSE'.encode('ascii'))
+                client.close()
+                continue
+
         usernames.append(username)
         clients.append(client)
 
@@ -60,6 +88,18 @@ def receive():
         # Handle multiple client requests at the same time by using threads simultaneously
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
+
+
+def kick_user(name):
+    if name in usernames:
+        # print('in')
+        name_index = usernames.index(name)
+        client_to_kick = clients[name_index]
+        clients.remove(client_to_kick)
+        client_to_kick.send("You have been kicked by the admin!".encode('ascii'))
+        usernames.remove(name)
+        announce((name + " was kicked by the admin!").encode('ascii'))
+        client_to_kick.close()
 
 
 print("Server is up and running...")
